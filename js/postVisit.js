814dc203d1288c2ce8178d55c189849423d6837b
Benjamin Eskildsen <eskildsen.ben@gmail.com>
2023-03-23 10:54:07 -0400

    fix postVisit

--- js/postVisit.js
+++ js/postVisit.js
@@ -1,4 +1,3 @@
-
 import axios from 'axios';
 // const axios = require('axios');
 
@@ -9,17 +8,20 @@ const axiosInstance = axios.create({
 
 
 const postVisit = (path = '/index', map = 'GET') => {
-  const isUnique = !!!localStorage.getItem('isRevisit');
-  localStorage.setItem('isRevisit', true);
+  const hostname = getHostname();
+  const isUnique = !!!localStorage.getItem('isRevisit_' + hostname);
+  localStorage.setItem('isRevisit_' + hostname, true);
   return axiosInstance
     .post('/visit', {
-      hostname: 'benGPT', path, isUnique, map,
+      hostname, path, isUnique, map,
     })
 };
 
 
 const getHostname = () => {
-  return window.location.hostname;
+  // return window.location.hostname;
+  return 'benGPT';
 }
 
 export default postVisit;
+
